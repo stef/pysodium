dcdc415f6fde75ac7d3bdb6bc54c69ef8d385426
stef <stefan.marsiske@gmail.com>
2013-08-26 21:38:13 +0200

    [mod] ver++

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "pysodium",
-    version = "0.5.3",
+    version = "0.5.4",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     description = ("python libsodium wrapper"),
