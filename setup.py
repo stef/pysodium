a128e55a1a5643c8b8ea1e8cd53e89d15324004c
stef <stefan.marsiske@gmail.com>
2013-09-07 01:51:46 +0200

    [mod] bump v

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "pysodium",
-    version = "0.5.8",
+    version = "0.5.9",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     description = ("python libsodium wrapper"),
