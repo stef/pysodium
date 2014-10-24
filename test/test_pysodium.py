6e35c7c9a0397bf279fab71616471c5e840f5370
Bulat Gaifullin <gaifullinbf@gmail.com>
2014-10-24 10:47:03 +0400

    fixed copyright

--- test/test_pysodium.py
+++ test/test_pysodium.py
@@ -1,9 +1,30 @@
+#!/usr/bin/env python2
 """
-@file
-@brief    
-@details  Copyright (c) 2001-2014 Acronis
-@author   Bulat Gaifullin (bulat.gaifullin@acronis.com)
-@since    $Id: $
+Wrapper for libsodium library
+
+Copyright (c) 2013-2014, Marsiske Stefan.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import pysodium
@@ -59,7 +80,7 @@ class TestPySodium(unittest.TestCase):
         self.assertRaises(ValueError, pysodium.crypto_sign_open, changed, pk)
 
     def test_crypto_sign_seed_keypair(self):
-        seed = pysodium.crypto_generichash('howdy', outlen=pysodium.crypto_sign_SEEDBYTES)
+        seed = pysodium.crypto_generichash(b'howdy', outlen=pysodium.crypto_sign_SEEDBYTES)
         pk, sk = pysodium.crypto_sign_seed_keypair(seed)
         pk2, sk2 = pysodium.crypto_sign_seed_keypair(seed)
         self.assertEqual(pk, pk2)
@@ -82,3 +103,7 @@ class TestPySodium(unittest.TestCase):
         output = pysodium.crypto_stream_chacha20_xor(input_, nonce, key)
         self.assertEqual(binascii.unhexlify(b"f798a189f195e66982105ffb640bb7757f579da31602fc93ec01ac56f85ac3c134a4547b733b46413042c9440049176905d3be59ea1c53f15916155c2be8241a38008b9a26bc35941e2444177c8ade6689de95264986d95889fb60e84629c9bd9a5acb1cc118be563eb9b3a4a472f82e09a7e778492b562ef7130e88dfe031c79db9d4f7c7a899151b9a475032b63fc385245fe054e3dd5a97a5f576fe064025d3ce042c566ab2c507b138db853e3d6959660996546cc9c4a6eafdc777c040d70eaf46f76dad3979e5c5360c3317166a1c894c94a371876a94df7628fe4eaaf2ccb27d5aaae0ad7ad0f9d4b6ad3b54098746d4524d38407a6deb3ab78fab78c9"),
                          output)
+
+
+if __name__ == '__main__':
+    unittest.main()
