cf8231d75d55c0abf4f2399976b5f384443f6dca
Bulat Gaifullin <gaifullinbf@gmail.com>
2014-10-27 23:15:38 +0300

    added dependencies to libsodium

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os 
 from setuptools import setup, find_packages
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -9,19 +10,19 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pysodium",
-    version = "0.6.5.1",
-    author = "Stefan Marsiske",
-    author_email = "s@ctrlc.hu",
-    description = ("python libsodium wrapper"),
-    license = "BSD",
-    keywords = "cryptography API NaCl libsodium",
-    url = "https://github.com/stef/pysodium",
-    packages = find_packages(),
+    name="pysodium",
+    version="0.6.5.1",
+    author="Stefan Marsiske",
+    author_email="s@ctrlc.hu",
+    description="python libsodium wrapper",
+    license="BSD",
+    keywords="cryptography API NaCl libsodium",
+    url="https://github.com/stef/pysodium",
+    packages=find_packages(),
     long_description=read('README.md'),
-    classifiers = ["Development Status :: 4 - Beta",
-                   "License :: OSI Approved :: BSD License",
-                   "Topic :: Security :: Cryptography",
-                   "Topic :: Security",
-                   ],
+    requires=["libsodium"],
+    classifiers=["Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: BSD License",
+                 "Topic :: Security :: Cryptography",
+                 "Topic :: Security"],
 )
