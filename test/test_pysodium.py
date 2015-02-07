0e6d355ad2b85d3dd64ea4e40b8ffac48fc9c74c
Cale Black <cablack@rams.colostate.edu>
2015-02-07 16:13:35 -0700

    Added crypto_pwhash_scryptsalsa208sha256 support for key derivation.

--- test/test_pysodium.py
+++ test/test_pysodium.py
@@ -104,6 +104,15 @@ class TestPySodium(unittest.TestCase):
         self.assertEqual(binascii.unhexlify(b"f798a189f195e66982105ffb640bb7757f579da31602fc93ec01ac56f85ac3c134a4547b733b46413042c9440049176905d3be59ea1c53f15916155c2be8241a38008b9a26bc35941e2444177c8ade6689de95264986d95889fb60e84629c9bd9a5acb1cc118be563eb9b3a4a472f82e09a7e778492b562ef7130e88dfe031c79db9d4f7c7a899151b9a475032b63fc385245fe054e3dd5a97a5f576fe064025d3ce042c566ab2c507b138db853e3d6959660996546cc9c4a6eafdc777c040d70eaf46f76dad3979e5c5360c3317166a1c894c94a371876a94df7628fe4eaaf2ccb27d5aaae0ad7ad0f9d4b6ad3b54098746d4524d38407a6deb3ab78fab78c9"),
                          output)
 
+    def test_crypto_pwhash_scryptsalsa208sha256(self):
+        passwd = "howdy"
+        outlen = 128
+        salt = pysodium.randombytes(pysodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES);
+        output = pysodium.crypto_pwhash_scryptsalsa208sha256(outlen, passwd, salt)
+        self.assertEqual(128, len(output))
+        salt = b'12345678901234567890123456789012'
+        output = pysodium.crypto_pwhash_scryptsalsa208sha256(outlen, passwd, salt)
+        self.assertEqual(binascii.unhexlify(b'6af52bde42cb37deae6661ba684c78d006ed450542e812a7133f14d052fb59bd400476d2787834df1ab47576cb18da4881e5cea2cba4b676e2abc5411af80d5ddcf36217b43b3eab50a0067e820b5bcc215be0fc6ff016a717ff876304d51a87af7e8d0113f737a268e94a76abd4e690b0012a688e895e7edb93a030cd341b4f'), output)
 
 if __name__ == '__main__':
     unittest.main()
