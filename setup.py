04fa61e282fc2faa8e825c0daddd0ddd9be559cb
stf <s@ctrlc.hu>
2015-11-11 17:06:27 +0100

    [mod] bumped version to 0.6.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="pysodium",
-    version="0.6.7",
+    version="0.6.8",
     author="Stefan Marsiske",
     author_email="s@ctrlc.hu",
     description="python libsodium wrapper",
