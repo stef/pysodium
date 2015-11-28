4bd43fdac674746c993c8e07df49bc5020e3b2e5
stf <s@ctrlc.hu>
2015-11-28 22:57:56 +0100

    [fix] #32 ad cannot be none in crypto_aead_chacha20poly1305_*crypt

--- pysodium/__init__.py
+++ pysodium/__init__.py
@@ -126,6 +126,7 @@ def crypto_stream_chacha20_xor(message, nonce, key):
 # crypto_aead_chacha20poly1305_encrypt(unsigned char *c, unsigned long long *clen, const unsigned char *m, unsigned long long mlen, const unsigned char *ad, unsigned long long adlen, const unsigned char *nsec, const unsigned char *npub, const unsigned char *k);
 def crypto_aead_chacha20poly1305_encrypt(message, ad, nonce, key):
 
+    if ad == None: raise ValueError("ad cannot be None")
     mlen = ctypes.c_ulonglong(len(message))
 
     if ad:
@@ -143,6 +144,7 @@ def crypto_aead_chacha20poly1305_encrypt(message, ad, nonce, key):
 # crypto_aead_chacha20poly1305_decrypt(unsigned char *m, unsigned long long *mlen, unsigned char *nsec, const unsigned char *c, unsigned long long clen, const unsigned char *ad, unsigned long long adlen, const unsigned char *npub, const unsigned char *k)
 def crypto_aead_chacha20poly1305_decrypt(ciphertext, ad, nonce, key):
 
+    if ad == None: raise ValueError("ad cannot be None")
     m = ctypes.create_string_buffer(len(ciphertext) - 16)
     mlen = ctypes.c_ulonglong(0)
     clen = ctypes.c_ulonglong(len(ciphertext))
