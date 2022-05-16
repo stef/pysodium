22b070e1f828ebd0a58cd63ae52a305b0b2b3b52
Andreas Schneider <asn@cryptomilk.org>
2022-05-16 13:32:00 +0200

    Do not install tests
    The tests should only be in the source tarball and not be installed.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     license="BSD",
     keywords="cryptography API NaCl libsodium",
     url="https://github.com/stef/pysodium",
-    packages=find_packages(),
+    packages=find_packages(exclude=['test*']),
     long_description=read('README.md'),
     requires=["libsodium"],
     classifiers=["Development Status :: 4 - Beta",
